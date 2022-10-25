1698ca164ac91e984dc1ae4b8e1134b5ccefac92
lwu1822 <lwu1822@gmail.com>
2022-10-24 21:57:41 -0700

    change haha and boohoo to yes and no

--- model_jokes.py
+++ model_jokes.py
@@ -15,7 +15,7 @@ def initJokes():
     # setup jokes into a dictionary with id, joke, haha, boohoo
     item_id = 0
     for item in feedback_list:
-        feedback_data.append({"id": item_id, "joke": item, "haha": 0, "boohoo": 0})
+        feedback_data.append({"id": item_id, "feedback": item, "yes": 0, "no": 0})
         item_id += 1
     # prime some haha responses
     for i in range(10):
@@ -43,8 +43,8 @@ def favoriteJoke():
     best = 0
     bestID = -1
     for joke in getJokes():
-        if joke['haha'] > best:
-            best = joke['haha']
+        if joke['yes'] > best:
+            best = joke['yes']
             bestID = joke['id']
     return feedback_data[bestID]
     
@@ -53,24 +53,24 @@ def jeeredJoke():
     worst = 0
     worstID = -1
     for joke in getJokes():
-        if joke['boohoo'] > worst:
-            worst = joke['boohoo']
+        if joke['no'] > worst:
+            worst = joke['no']
             worstID = joke['id']
     return feedback_data[worstID]
 
 # Add to haha for requested id
 def addJokeHaHa(id):
-    feedback_data[id]['haha'] = feedback_data[id]['haha'] + 1
-    return feedback_data[id]['haha']
+    feedback_data[id]['yes'] = feedback_data[id]['yes'] + 1
+    return feedback_data[id]['yes']
 
 # Add to boohoo for requested id
 def addJokeBooHoo(id):
-    feedback_data[id]['boohoo'] = feedback_data[id]['boohoo'] + 1
-    return feedback_data[id]['boohoo']
+    feedback_data[id]['no'] = feedback_data[id]['no'] + 1
+    return feedback_data[id]['no']
 
 # Pretty Print joke
 def printJoke(joke):
-    print(joke['id'], joke['joke'], "\n", "haha:", joke['haha'], "\n", "boohoo:", joke['boohoo'], "\n")
+    print(joke['id'], joke['feedback'], "\n", "yes:", joke['yes'], "\n", "no:", joke['no'], "\n")
 
 # Number of jokes
 def countJokes():
@@ -82,10 +82,10 @@ if __name__ == "__main__":
     
     # Most likes and most jeered
     best = favoriteJoke()
-    print("Most liked", best['haha'])
+    print("Most liked", best['yes'])
     printJoke(best)
     worst = jeeredJoke()
-    print("Most jeered", worst['boohoo'])
+    print("Most jeered", worst['no'])
     printJoke(worst)
     
     # Random joke
